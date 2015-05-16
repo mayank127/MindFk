95ab3614fe5a4e8c0fcd2f3f49cf35ef6d1923d0
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2015-05-17 01:32:22 +0530

    changes for heroku

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var fs = require('fs');
 var express = require('express');
-var session = require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var flash = require('connect-flash');
 
 var env = process.env.NODE_ENV || 'default';
 var config = require('config');
@@ -18,12 +13,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser('S3CRE7'));
-app.use(flash());
-app.use(session({ secret: 'S3CRE7-S3SSI0N', saveUninitialized: true, resave: true } ));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // configure routes
