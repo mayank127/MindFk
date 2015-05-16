5efc09890e950ea0a819416712e61a657d76c382
Mayank Meghwanshi <mayankmeghwanshi@gmail.com>
2015-05-17 01:51:35 +0530

    getting port from process

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var routes = require('./routes/index');
 app.use('/', routes);
 
 // launch app server
-var server = require('http').createServer(app).listen(80);
+var server = require('http').createServer(app).listen(process.env.PORT || 3000);
 
 require('./config/socket.js')(server);
 
